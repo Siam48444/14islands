2c248c1d182a5231f34891bc347731d97e02bebe
Siam48444 <siam48444@gmail.com>
2024-01-08 20:09:06 +0600

    j

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ tl.from(".main_top_content", {
 });
 
 // loaded after the hero section.
-tl.from("#area2, #area3, #area4, #area5, #area6, footer", {
+tl.from("#area2, #area3, #area4, #area5, #area6, #area7, footer", {
    display: "none",
    opacity: 0,
    duration: 0.5,
